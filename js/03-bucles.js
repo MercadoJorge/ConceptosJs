50bdebe268a5ee455e6bfa0eba04a04de9068ba8
Jorge Mercado <mercadojorge0@gmail.com>
2024-07-08 01:03:53 -0300

    agregamos un ejemplo de while

---
+++ js/03-bucles.js
@@ -0,0 +1,16 @@
+// estructuras repetitivas
+
+//estrucutra while
+
+/*
+while(condicion logica){
+aqui agrego todo el codigo que quiere repetir varias veces
+agregar un codigo que haga que la condicion logica se deje de cumplir
+}
+*/
+let renglon = 1;// mi variable renglon vale 1
+
+while(renglon <= 10){// si mi variable vale 10 o igual a 10
+    document.write(`<p>hola mundo ${renglon}</p>`)//escribo hola mundo ${1 el valor de mi variable renglon}
+    renglon = renglon +1 // si las condiciones se cumplen mi variable renglon es igual a renglon +1
+}
