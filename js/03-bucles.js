e1156c1dd45d40342996f39d89cab49ecb9a2508
Jorge Mercado <mercadojorge0@gmail.com>
2024-07-09 12:34:32 -0300

    se agrega contenido a la pagina bucles

--- js/03-bucles.js
+++ js/03-bucles.js
@@ -1,6 +1,6 @@
 // estructuras repetitivas
 
-//estrucutra while
+//ESTRUCTURA WHILE
 
 /*
 while(condicion logica){
@@ -8,9 +8,20 @@ aqui agrego todo el codigo que quiere repetir varias veces
 agregar un codigo que haga que la condicion logica se deje de cumplir
 }
 */
+
 let renglon = 1;// mi variable renglon vale 1
 
 while(renglon <= 10){// si mi variable vale 10 o igual a 10
     document.write(`<p>hola mundo ${renglon}</p>`)//escribo hola mundo ${1 el valor de mi variable renglon}
     renglon = renglon +1 // si las condiciones se cumplen mi variable renglon es igual a renglon +1
 }
+
+// se puede resumir el codigo anterior 
+
+let renglon2 = 1;// mi variable renglon vale 1
+
+while(renglon2 <= 10){// si mi variable vale 10 o igual a 10
+    document.write(`<p>hola mundo ${renglon2}</p>`)//escribo hola mundo ${1 el valor de mi variable renglon}
+    renglon2++}// se puede resumir el codigo agregando dos ++ para aumentar o dos -- para restar 
+
+    // ESTRUCTURA DO WHILE
