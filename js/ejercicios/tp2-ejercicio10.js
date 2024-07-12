0b0d89f062c31b8b141a1612a8c670faa7acca0e
Jorge Mercado <mercadojorge0@gmail.com>
2024-07-12 20:52:33 -0300

    se agregan contenido al ejercicio de practica

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -2,9 +2,10 @@ const filas = parseInt(prompt(`Ingrese un numero de filas`));
 const columnas = parseInt(prompt(`Ingrese un numero de columnas`));
 
 document.write(`<table><tbody>`);
-document.write(`<tr>`);
+for(let indiceFilas = 1; indiceFilas <= filas; indiceFilas ++){
+  document.write(`<tr>`);
 
-for(let indiceColumnas = 1;indiceColumnas <= columnas; indiceColumnas ++)
-document.write(`<td>1</td>`);
+for (let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas ++)
+  document.write(`<td>1</td>`)};
 document.write(`</tr>`);
 document.write(`</tbody></table>`);
