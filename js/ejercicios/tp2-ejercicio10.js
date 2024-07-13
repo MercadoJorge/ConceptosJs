e27c454668bb3220e9c499fbbce9b54ed833bf62
Jorge Mercado <mercadojorge0@gmail.com>
2024-07-12 21:00:24 -0300

    agregamos los valores en la tabla

--- js/ejercicios/tp2-ejercicio10.js
+++ js/ejercicios/tp2-ejercicio10.js
@@ -1,11 +1,15 @@
 const filas = parseInt(prompt(`Ingrese un numero de filas`));
 const columnas = parseInt(prompt(`Ingrese un numero de columnas`));
+let totalceldas = filas * columnas;
 
 document.write(`<table><tbody>`);
-for(let indiceFilas = 1; indiceFilas <= filas; indiceFilas ++){
+for (let indiceFilas = 1; indiceFilas <= filas; indiceFilas++) {
   document.write(`<tr>`);
 
-for (let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas ++)
-  document.write(`<td>1</td>`)};
+  for (let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++){
+    document.write(`<td>${totalceldas}</td>`);
+  //decrementar
+  totalceldas--};
+}
 document.write(`</tr>`);
 document.write(`</tbody></table>`);
